b212859ffe5a4b888cf6cf7a72e63b2c3b80f0d5
MenassieErmias <ermiasmenase@gmail.com>
2024-11-19 15:47:39 +0300

    edit services function made, uuid installed as dependancy, imported getAllServices and editService from controller to api: controller/services.js, package.json, api/service.json

--- api/services.js
+++ api/services.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import { createService, editService } from '../controller/services.js';
+
 const servicesRouter = express.Router();
 
 servicesRouter.get('/', getAllServices);
+servicesRouter.put('/:id', editService);
--- controller/services.js
+++ controller/services.js
@@ -1,3 +1,4 @@
+import v4 from 'uuid'
 let services = [];
 
 const getAllServices = (req, res) => {
@@ -8,6 +9,7 @@ const createService = (req, res) => {
   const { name, imageURL, desc } = req.body;
 
   const newService = {
+    id: v4(),
     name: name,
     imageURL: imageURL,
     desc: desc
@@ -15,3 +17,21 @@ const createService = (req, res) => {
 
   services = [...services, newService];
 }
+
+const editService = (req, res) => {
+  const id = req.params.id; // TODO: need to make sure where id comes from
+
+  const updatedService = services.find(service => service.id === id);
+
+  console.log(`updatedService ${updatedService}`);
+
+  const { name, imageURL, desc } = req.body;
+
+  if (name) updatedService.name = name;
+  if (imageURL) updatedService.imageURL = imageURL;
+  if (desc) updatedService.desc = desc;
+
+}
+
+
+export default { createService, editService }
