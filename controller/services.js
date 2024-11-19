4aee60f06c5b4e2a179b863ba78523725374dd9f
MenassieErmias <ermiasmenase@gmail.com>
2024-11-19 15:37:11 +0300

    createService in controller: controller/sercvices.js

--- controller/services.js
+++ controller/services.js
@@ -4,4 +4,14 @@ const getAllServices = (req, res) => {
   res.json(services);
 }
 
-// const createService = (req, res) =>
+const createService = (req, res) => {
+  const { name, imageURL, desc } = req.body;
+
+  const newService = {
+    name: name,
+    imageURL: imageURL,
+    desc: desc
+  }
+
+  services = [...services, newService];
+}
